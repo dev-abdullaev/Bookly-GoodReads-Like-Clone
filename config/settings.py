d70d3521f21bce3ba99b8a1c68d6600f01a3c795
Zokhid Abdullaev <77606851+dev-abdullaev@users.noreply.github.com>
2022-03-26 15:43:55 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -155,12 +155,4 @@ REST_FRAMEWORK = {
 }
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'developer.abdullaev@gmail.com'
-EMAIL_HOST_PASSWORD = 'password'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-
 
